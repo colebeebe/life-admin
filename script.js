86ab6af20276edfce1e0684910e626ac47f465b5
colebeebe <bee14003@byui.edu>
2025-10-26 09:19:09 -0400

    Worked on button mechanics

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ const button = document.querySelector("#menu-button");
 
 button.addEventListener('click', () => {
     document.querySelector("main").classList.toggle("open");
+    button.classList.toggle("open");
+    if (button.classList.contains("open")) {
+	button.innerHTML = "<";
+    }
+    else {
+	button.innerHTML = "☰";
+    }
 });
