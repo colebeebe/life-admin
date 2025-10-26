4bf6b96859785689328bdd1af4356129d313aa8c
colebeebe <bee14003@byui.edu>
2025-10-26 14:03:21 -0400

    Menu actually appears now

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.querySelector("#menu-button");
 
 button.addEventListener('click', () => {
     document.querySelector("main").classList.toggle("open");
+    document.querySelector("#side-panel").classList.toggle("open");
     button.classList.toggle("open");
     if (button.classList.contains("open")) {
 	button.innerHTML = "<";
