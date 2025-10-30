94d5da506731ed45f9d0b57be8c701bc2e4649da
colebeebe <bee14003@byui.edu>
2025-10-30 18:45:36 -0400

    Notes are now dynamically added to the DOM

--- notes.js
+++ notes.js
@@ -1,13 +1,82 @@
-const textarea = document.getElementById("note-input");
+const notes = [
+    {
+	id: 1,
+	title: "Notes on CSE 310 for module03",
+	date: new Date(2025, 9, 30),
+	content: "There are many things that still need to be worked on. A few of these things are as follows:<br><br> <ul> <li>look into the ability to save things locally</li> <li>Flesh out CRUD</li> <li>Implement a landing \"Home\" page</li> </ul>"
+    },
+    {
+	id: 2,
+	title: "A Secondary Note",
+	date: null,
+	content: "This is just a secondary note for now, just to make sure that spacing and other formatting things look ok."
+    },
+    {
+	id: 3,
+	title: "",
+	date: null,
+	content: "This is a tertiary note to test what it may look like without a title or date"
+    },
+    {
+	id: 4,
+	title: "Lorem Ipsum",
+	date: new Date(2025, 9, 30),
+	content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+	id: 5,
+	title: "",
+	date: new Date(2025, 9, 30),
+	content: "This is a final note to see what it might look like with just a date"
+    }
+];
 
-document.addEventListener('keydown', function(event) {
-    if (document.activeElement !== textarea) {
-	textarea.focus();
+const main = document.querySelector("main");
 
-	if (event.key.length === 1) {
-	    textarea.value += event.key;
-	}
-
-	event.preventDefault();
-    } 
+// The formatter for the date
+const formatter = Intl.DateTimeFormat("en-US", {
+    month: "numeric",
+    day: "numeric",
+    year: "numeric"
 });
+
+notes.forEach((note) => {
+    const noteItem = document.createElement("div");
+    noteItem.classList.add("note-item");
+
+    const title = document.createElement("h1");
+    title.innerHTML = note.title;
+    const date = document.createElement("h2");
+
+    if (note.date !== null) {
+	date.innerHTML = formatter.format(note.date);
+    }
+
+    if (note.title !== "" || note.date !== null) {
+	const header = document.createElement("div");
+	header.classList.add("note-header");
+	header.appendChild(title);
+	header.appendChild(date);
+	noteItem.appendChild(header);
+    }
+
+    const content = document.createElement("p");
+    content.innerHTML = note.content;
+    noteItem.appendChild(content);
+
+    main.appendChild(noteItem);
+});
+
+// const textarea = document.getElementById("note-input");
+// 
+// document.addEventListener('keydown', function(event) {
+//     if (document.activeElement !== textarea) {
+// 	textarea.focus();
+// 
+// 	if (event.key.length === 1) {
+// 	    textarea.value += event.key;
+// 	}
+// 
+// 	event.preventDefault();
+//     } 
+// });
